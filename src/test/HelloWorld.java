7859efe6305dacaa3ac0009ea2f4f7bc2aba30ca
tpdus751 <152615034+tpdus751@users.noreply.github.com>
2024-05-10 15:01:14 +0900

    Update HelloWorld.java
    add message from github

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git World~~~~");
-		System.out.println("깃허브 연동 성공~!");
+		System.out.println("깃허브 연동 성공~! 진짜로??");
 	}
 
 }
